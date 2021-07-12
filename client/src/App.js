677c2051103be1ed33bc62e9a1c7e1841e3abea9
iiMox <iimox04@gmail.com>
2021-07-12 22:03:38 +0100

    Js code

--- client/src/App.js
+++ client/src/App.js
@@ -5,18 +5,42 @@ import { useState, useEffect } from "react";
 import "./App.css";
 
 const App = () => {
-    const [newTask, setNewTask] = useState("");
+    const [newTask, setNewTask] = useState(null);
     const [tasks, setTasks] = useState([]);
 
     useEffect(() => {
-        setTasks(localStorage.getItem("tasks"));
+        setTasks(JSON.parse(localStorage.getItem("tasks")));
     }, []);
 
     const onClick = (e) => {
         e.preventDefault();
-        setTasks([newTask, ...tasks]);
-        localStorage.setItem("tasks", tasks);
+        const taskArray =
+            localStorage.getItem("tasks") !== null
+                ? JSON.parse(localStorage.getItem("tasks"))
+                : [];
+        taskArray.push({
+            ...newTask,
+            id: Math.random() * 10000000000,
+            checked: false,
+        });
+        localStorage.setItem("tasks", JSON.stringify(taskArray));
+        setTasks(JSON.parse(localStorage.getItem("tasks")));
     };
+
+    const outPut = [];
+    let i = 0;
+    tasks?.forEach((task) => {
+        outPut.push(
+            <TaskBox
+                key={i}
+                title={task.title}
+                date={task.date}
+                checked={task.checked}
+                id={task.id}
+            />
+        );
+        i++;
+    });
 
     return (
         <div>
@@ -27,15 +51,38 @@ const App = () => {
                         <input
                             type="text"
                             placeholder="Write your task"
-                            onChange={(e) => setNewTask(e.target.value)}
+                            onChange={(e) =>
+                                setNewTask({
+                                    ...newTask,
+                                    title: e.target.value,
+                                })
+                            }
+                        />
+                        <input
+                            type="date"
+                            onChange={(e) =>
+                                setNewTask({ ...newTask, date: e.target.value })
+                            }
                         />
                         <button onClick={(e) => onClick(e)}>Add</button>
                     </form>
                 </div>
                 <hr />
                 <div className="my-tasks">
-                    <TaskBox />
-                    <TaskBox />
+                    {outPut.length !== 0 ? (
+                        outPut
+                    ) : (
+                        <div
+                            style={{
+                                textAlign: "center",
+                                color: "#ababab",
+                                margin: "18px auto 10px",
+                                letterSpacing: "0.8px",
+                            }}
+                        >
+                            No Tasks For Today.
+                        </div>
+                    )}
                 </div>
             </div>
         </div>
--- client/src/components/TaskBox/index.js
+++ client/src/components/TaskBox/index.js
@@ -1,12 +1,40 @@
 import React from "react";
+import { useState, useEffect } from "react";
 
 import "./TaskBox.css";
 
-const TaskBox = () => {
-    return (
+const TaskBox = ({ id, title, date, checked }) => {
+    const [task, setTask] = useState(null);
+    const [tasks, setTasks] = useState([]);
+    useEffect(() => {
+        setTask({ id, title, date, checked });
+        setTasks(JSON.parse(localStorage.getItem("tasks")));
+    }, []);
+    return task ? (
         <div className="task-box">
-            <input type="checkbox" />
-            <div className="task-title">Complete the cours</div>
+            <input
+                type="checkbox"
+                checked={task.checked}
+                onChange={(e) => {
+                    const array = [];
+                    tasks.forEach((task) => {
+                        if (task.id === id) {
+                            array.push({
+                                id,
+                                title,
+                                date,
+                                checked: e.target.checked,
+                            });
+                        } else {
+                            array.push(task);
+                        }
+                    });
+                    setTasks(array);
+                    localStorage.setItem("tasks", JSON.stringify(tasks));
+                    setTask({ ...task, checked: e.target.checked });
+                }}
+            />
+            <div className="task-title">{task.title}</div>
             <div className="actions">
                 <div className="edit-task">
                     <i className="far fa-edit"></i>
@@ -16,7 +44,7 @@ const TaskBox = () => {
                 </div>
             </div>
         </div>
-    );
+    ) : null;
 };
 
 export default TaskBox;
