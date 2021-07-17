821b21534846dca5876ffeb28c71a80381630b8c
iiMox <iimox04@gmail.com>
2021-07-17 21:23:28 +0100

    Completing the app

--- client/src/App.js
+++ client/src/App.js
@@ -12,6 +12,7 @@ const App = () => {
     const [newTask, setNewTask] = useState({ checked: false });
     const [reload, setReload] = useState(false);
     const [day, setDay] = useState(new Date());
+    const [erreur, setErreur] = useState("");
 
     useEffect(() => {
         if (localStorage.getItem("tasks")) {
@@ -25,11 +26,24 @@ const App = () => {
 
     const onClick = (e) => {
         e.preventDefault();
-        dispatch({ type: "ADD_TASK", payload: newTask });
-        localStorage.setItem("tasks", JSON.stringify([...tasks, newTask]));
-        setReload(true);
+        if (
+            newTask.title === undefined ||
+            newTask.date === undefined ||
+            newTask.title === "" ||
+            newTask.date === ""
+        ) {
+            setErreur("Field is empty");
+            setTimeout(() => setErreur(""), 1000);
+        } else {
+            dispatch({ type: "ADD_TASK", payload: newTask });
+            localStorage.setItem("tasks", JSON.stringify([...tasks, newTask]));
+            setReload(true);
+        }
     };
 
+    let nbrTasks = 0;
+    let nbrTasksDone = 0;
+    let nbrTasksWaits = 0;
     const outPut = [];
     const stToday = `${day.getFullYear()}-0${
         day.getMonth() + 1
@@ -38,6 +52,12 @@ const App = () => {
     tasks.forEach((task) => {
         if (task.date === stToday) {
             outPut.push(<TaskBox key={i} task={task} />);
+            nbrTasks++;
+            if (task.checked) {
+                nbrTasksDone++;
+            } else {
+                nbrTasksWaits++;
+            }
             i++;
         }
     });
@@ -56,68 +76,93 @@ const App = () => {
     return (
         <div>
             <h1>My Todo List</h1>
-            <div className="content">
-                <div className="add-task">
-                    <form>
-                        <input
-                            type="text"
-                            placeholder="Write your task"
-                            onChange={(e) =>
-                                setNewTask({
-                                    ...newTask,
-                                    title: e.target.value,
-                                    id: parseInt(Math.random() * 10000000000),
-                                })
-                            }
-                        />
-                        <input
-                            type="date"
-                            onChange={(e) =>
-                                setNewTask({ ...newTask, date: e.target.value })
-                            }
-                        />
-                        <button onClick={onClick}>Add</button>
-                    </form>
-                </div>
-                <hr />
-                <div className="control-date">
-                    <div
-                        className="prev"
-                        onClick={() => {
-                            const d = new Date();
-                            d.setDate(day.getDate() - 1);
-                            setDay(d);
-                        }}
-                    >
-                        <i className="fas fa-chevron-left"></i>
+            <div className="flex-container">
+                <div className="content">
+                    <div className="add-task">
+                        <form>
+                            <input
+                                type="text"
+                                placeholder="Write your task"
+                                onChange={(e) =>
+                                    setNewTask({
+                                        ...newTask,
+                                        title: e.target.value,
+                                        id: parseInt(
+                                            Math.random() * 10000000000
+                                        ),
+                                    })
+                                }
+                            />
+                            <input
+                                type="date"
+                                onChange={(e) =>
+                                    setNewTask({
+                                        ...newTask,
+                                        date: e.target.value,
+                                    })
+                                }
+                            />
+                            <button onClick={onClick}>Add</button>
+                        </form>
                     </div>
-                    <div className="actual">{whatToRender()}</div>
-                    <div
-                        className="next"
-                        onClick={() => {
-                            const d = new Date();
-                            d.setDate(day.getDate() + 1);
-                            setDay(d);
-                        }}
-                    >
-                        <i className="fas fa-chevron-right"></i>
+                    <div className={`erreur ${erreur !== "" ? "visible" : ""}`}>
+                        {erreur}
                     </div>
-                </div>
-                <div className="my-tasks">
-                    {outPut.length !== 0 ? (
-                        outPut.reverse()
-                    ) : (
+                    <hr />
+                    <div className="control-date">
                         <div
-                            style={{
-                                textAlign: "center",
-                                color: "#ababab",
-                                margin: "18px auto 10px",
-                                letterSpacing: "0.8px",
+                            className="prev"
+                            onClick={() => {
+                                const d = new Date();
+                                d.setDate(day.getDate() - 1);
+                                setDay(d);
                             }}
                         >
-                            No Tasks For Today.
+                            <i className="fas fa-chevron-left"></i>
                         </div>
-                    )}
+                        <div className="actual">{whatToRender()}</div>
+                        <div
+                            className="next"
+                            onClick={() => {
+                                const d = new Date();
+                                d.setDate(day.getDate() + 1);
+                                setDay(d);
+                            }}
+                        >
+                            <i className="fas fa-chevron-right"></i>
+                        </div>
+                    </div>
+                    <div className="my-tasks">
+                        {outPut.length !== 0 ? (
+                            outPut.reverse()
+                        ) : (
+                            <div
+                                style={{
+                                    textAlign: "center",
+                                    color: "#ababab",
+                                    margin: "18px auto 10px",
+                                    letterSpacing: "0.8px",
+                                }}
+                            >
+                                No Tasks For Today.
+                            </div>
+                        )}
+                    </div>
+                </div>
+                <div className="stats">
+                    <h2>Statistics</h2>
+                    <hr />
+                    <ul>
+                        <li>
+                            Total tasks today : <span>{nbrTasks}</span>
+                        </li>
+                        <li>
+                            Tasks completed : <span>{nbrTasksDone}</span>
+                        </li>
+                        <li>
+                            Tasks waiting for you : <span>{nbrTasksWaits}</span>
+                        </li>
+                    </ul>
                 </div>
             </div>
         </div>
